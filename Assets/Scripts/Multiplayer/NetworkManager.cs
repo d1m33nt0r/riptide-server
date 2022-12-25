795a3bba726c1c03a189f230e1e18bcfc9f41cc6
dmytrohryb <dmitrii.9rib@gmail.com>
2022-12-25 12:50:48 +0200

    init

---
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,70 @@
+using RiptideNetworking;
+using RiptideNetworking.Utils;
+using UnityEngine;
+
+namespace Multiplayer
+{
+    public enum ServerToClientID : ushort
+    {
+        playerSpawned = 1
+    }
+    
+    public enum ClientToServerID : ushort
+    {
+        name = 1
+    }
+    
+    public class NetworkManager : MonoBehaviour
+    {
+        private static NetworkManager _singleton;
+
+        public static NetworkManager Singleton
+        {
+            get => _singleton;
+            private set
+            {
+                if (_singleton == null)
+                    _singleton = value;
+                else if (_singleton != value)
+                {
+                    Debug.Log($"{nameof(NetworkManager)} instance already exists, destroying duplicate!");
+                    Destroy(value);
+                }
+            }
+        }
+
+        public Server Server { get; private set; }
+
+        [SerializeField] private ushort port;
+        [SerializeField] private ushort maxClientsCount;
+        
+        private void Awake()
+        {
+            Singleton = this;
+        }
+
+        private void Start()
+        {
+            RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
+            
+            Server = new Server();
+            Server.Start(port, maxClientsCount);
+            Server.ClientDisconnected += PlayerLeft;
+        }
+
+        private void FixedUpdate()
+        {
+            Server.Tick();
+        }
+
+        private void OnApplicationQuit()
+        {
+            Server.Stop();
+        }
+
+        private void PlayerLeft(object sender, ClientDisconnectedEventArgs e)
+        {
+            Destroy(Player.list[e.Id].gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using Multiplayer;
+using RiptideNetworking;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Dictionary<ushort, Player> list = new ();
+    
+    public ushort ID { get; private set; }
+    public string Username { get; private set; }
+
+
+    private void OnDestroy()
+    {
+        list.Remove(ID);
+    }
+
+    public static void Spawn(ushort ID, string username)
+    {
+        foreach (var othePlayer in list.Values)
+        {
+            othePlayer.SendSpawned(ID);
+        }
+        
+        var player = Instantiate(GameLogic.Singleton.PlayerPrefab, new Vector3(0f, 1f, 0f), Quaternion.identity)
+            .GetComponent<Player>();
+        player.name = $"Player {ID} {(string.IsNullOrEmpty(username) ? "Guest" : username)}";
+        player.ID = ID;
+        player.Username = string.IsNullOrEmpty(username) ? $"Guest {ID}" : username;
+        player.SendSpawned();
+        list.Add(ID, player);
+    }
+
+    #region Messages
+
+    private void SendSpawned()
+    {
+        var message = Message.Create(MessageSendMode.reliable, (ushort)ServerToClientID.playerSpawned);
+        NetworkManager.Singleton.Server.SendToAll(AddSpawnData(message));
+    }
+
+    private void SendSpawned(ushort toClientID)
+    {
+        var message = Message.Create(MessageSendMode.reliable, (ushort)ServerToClientID.playerSpawned);
+        NetworkManager.Singleton.Server.Send(AddSpawnData(message), toClientID);
+    }
+
+    private Message AddSpawnData(Message message)
+    {
+        message.AddUShort(ID);
+        message.AddString(Username);
+        message.AddVector3(transform.position);
+        return message;
+    }
+    
+    [MessageHandler((ushort)ClientToServerID.name)]
+    public static void Name(ushort fromClientID, Message message)
+    {
+        Spawn(fromClientID, message.GetString());
+    }
+
+    #endregion
+    
+}
